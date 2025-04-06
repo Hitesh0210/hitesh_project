4806c4f9d2157ce35eb3a8b026583b1d4b2a9671
Hitesh0210 <95519343+Hitesh0210@users.noreply.github.com>
2025-04-06 18:51:11 +0530

    Update pyspark_dag.py

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -13,7 +13,7 @@ from airflow.providers.google.cloud.operators.dataproc import (
 PROJECT_ID = "hitesh-project-455614"
 REGION = "us-east1"
 CLUSTER_NAME = "my-demo-cluster"
-COMPOSER_BUCKET = "us-central1-demo-instance-9e2e6451-bucket"
+COMPOSER_BUCKET = "us-central1-demo-instance-3a13820f-bucket"
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/retailerMysqlToLanding.py"
 PYSPARK_JOB_1 = {
